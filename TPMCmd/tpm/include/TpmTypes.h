52014755adfcf91930c8a0b2dfeacaf51057708d
Andrey Marochko <Andrey.Marochko@microsoft.com>
2020-10-20 19:01:42 -0700

    Fixed the SPEC_VERSION and SPEC_DAY_OF_YEAR values.

--- TPMCmd/tpm/include/TpmTypes.h
+++ TPMCmd/tpm/include/TpmTypes.h
@@ -290,11 +290,11 @@ typedef UINT32                  TPM_SPEC;
 #define TPM_SPEC_FAMILY         (TPM_SPEC)(SPEC_FAMILY)
 #define SPEC_LEVEL              00
 #define TPM_SPEC_LEVEL          (TPM_SPEC)(SPEC_LEVEL)
-#define SPEC_VERSION            157
+#define SPEC_VERSION            159
 #define TPM_SPEC_VERSION        (TPM_SPEC)(SPEC_VERSION)
 #define SPEC_YEAR               2019
 #define TPM_SPEC_YEAR           (TPM_SPEC)(SPEC_YEAR)
-#define SPEC_DAY_OF_YEAR        283
+#define SPEC_DAY_OF_YEAR        312
 #define TPM_SPEC_DAY_OF_YEAR    (TPM_SPEC)(SPEC_DAY_OF_YEAR)
 
 // Table 2:7 - Definition of TPM_GENERATED Constants
